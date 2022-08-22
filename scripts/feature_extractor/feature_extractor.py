451d3e5539a7280cb07cfe94a978b5acb8b197b5
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2022-08-22 19:05:36 +0200

    made argparse a class

--- scripts/feature_extractor/feature_extractor.py
+++ scripts/feature_extractor/feature_extractor.py
@@ -4,6 +4,8 @@ import librosa
 import numpy as np
 import pickle
 
+from settings import feature_extractor_default_settings
+
 # In this particular case we ignore warnings of loading a .m4a audio
 # Not a good practice
 import warnings
@@ -11,89 +13,8 @@ warnings.filterwarnings("ignore")
 
 class FeatureExtractor:
 
-    def __init__(self):
-        self.parse_args()
-
-        
-    def initialize_parser(self):
-
-        self.parser = argparse.ArgumentParser(
-            description = 'Looks for audio files and extract features. \
-                It searches audio files in a paths file and dumps the  \
-                extracted features in the same directory in a .pickle format.',
-            )
-
-
-    def add_parser_args(self):
-
-        self.parser.add_argument(
-            'audio_paths_file_dir',
-            type = str, 
-            default = 'scripts/feature_extractor/feature_extractor_paths.lst',
-            help = '.lst file path containing the audio files paths we want to extract features from.',
-            )
-
-        self.parser.add_argument(
-            "--sampling_rate", "-sr", 
-            type = int,
-            default = 16000,
-            help = "Audio sampling rate (in Hz).",
-            )
-
-        self.parser.add_argument(
-            "--n_fft_secs", 
-            type = int,
-            default = 0.023,
-            help = "Length of the windowed signal after padding with zeros (in seconds).",
-            )
-
-        self.parser.add_argument(
-            "--window", 
-            type = str,
-            default = "hamming",
-            help = "Windowing function (librosa parameter).",
-            )
-
-        self.parser.add_argument(
-            "--win_length_secs", 
-            type = float,
-            default = 0.023,
-            help = "(In seconds). Each frame of audio is windowed by window of length win_length_secs and then padded with zeros to match n_fft_secs.",
-            )
-
-        self.parser.add_argument(
-            "--hop_length_secs", 
-            type = float,
-            default = 0.010,
-            help = "Hop length (in seconds).",
-            )
-
-        self.parser.add_argument(
-            "--pre_emph_coef", 
-            type = float,
-            default = 0.97,
-            help = "Pre-emphasis coefficient.",
-            )
-
-        self.parser.add_argument(
-            "--n_mels", 
-            type = int,
-            default = 80,
-            help = "Number of Mel bands to generate.",
-            )
-
-        self.parser.add_argument(
-            "--verbose", 
-            action = "store_true",
-            help = "Increase output verbosity.",
-            )
-
-        
-    def parse_args(self):
-
-        self.initialize_parser()
-        self.add_parser_args()
-        self.params = self.parser.parse_args()
+    def __init__(self, params):
+        self.params = params
 
 
     def count_input_lines(self):
@@ -139,7 +60,32 @@ class FeatureExtractor:
         return mel_spectrogram
 
 
-    def extract_features(self):
+    def extract_features(self, audio_path):
+
+        # Load the audio
+                
+        # TODO give the option to load with sf
+        # y, sfreq = sf.read('{}'.format(featureFile[:-1]))
+        # y, sfreq = sf.read('{}'.format(featureFile))
+        
+        samples, sampling_rate = librosa.load(
+            f'{audio_path}',
+            sr = self.params.sampling_rate,
+            mono = True, # converts to mono channel
+            ) 
+
+        assert int(sampling_rate) == int(self.params.sampling_rate)
+
+        # Create the log mel spectrogram
+        log_mel_spectrogram = self.generate_log_mel_spectrogram(
+            samples, 
+            self.params.sampling_rate,
+            )
+
+        return log_mel_spectrogram
+
+    
+    def main(self):
 
         self.count_input_lines()
 
@@ -154,26 +100,7 @@ class FeatureExtractor:
 
                 if self.params.verbose: print(f"[Feature Extractor] Processing file {audio_path}...")
 
-                # Load the audio
-                
-                # TODO give the option to load with sf
-                # y, sfreq = sf.read('{}'.format(featureFile[:-1]))
-                # y, sfreq = sf.read('{}'.format(featureFile))
-                
-                samples, sampling_rate = librosa.load(
-                    f'{audio_path}',
-                    sr = self.params.sampling_rate,
-                    mono = True, # converts to mono channel
-                    ) 
-
-                assert int(sampling_rate) == int(self.params.sampling_rate)
-
-                
-                # Create the log mel spectrogram
-                log_mel_spectrogram = self.generate_log_mel_spectrogram(
-                    samples, 
-                    self.params.sampling_rate,
-                    )
+                log_mel_spectrogram = self.extract_features(audio_path)
 
                 # Dump the spectrogram
                 # TODO fix this hardcoded -4
@@ -188,15 +115,100 @@ class FeatureExtractor:
                 line_num = line_num + 1
 
             print(f"[Feature Extractor] All audios processed!")
+
+
+class ArgsParser:
+
+    def __init__(self):
+        self.initialize_parser()
+
     
-    
+    def initialize_parser(self):
+
+        self.parser = argparse.ArgumentParser(
+            description = 'Looks for audio files and extract features. \
+                It searches audio files in a paths file and dumps the  \
+                extracted features in the same directory in a .pickle format.',
+            )
+
+
+    def add_parser_args(self):
+
+        self.parser.add_argument(
+            'audio_paths_file_dir',
+            type = str, 
+            default = feature_extractor_default_settings['audio_paths_file_dir'],
+            help = '.lst file path containing the audio files paths we want to extract features from.',
+            )
+
+        self.parser.add_argument(
+            "--sampling_rate", "-sr", 
+            type = int,
+            default = feature_extractor_default_settings['sampling_rate'],
+            help = "Audio sampling rate (in Hz).",
+            )
+
+        self.parser.add_argument(
+            "--n_fft_secs", 
+            type = int,
+            default = feature_extractor_default_settings['n_fft_secs'],
+            help = "Length of the windowed signal after padding with zeros (in seconds).",
+            )
+
+        self.parser.add_argument(
+            "--window", 
+            type = str,
+            default = feature_extractor_default_settings['window'],
+            help = "Windowing function (librosa parameter).",
+            )
+
+        self.parser.add_argument(
+            "--win_length_secs", 
+            type = float,
+            default = feature_extractor_default_settings['win_length_secs'],
+            help = "(In seconds). Each frame of audio is windowed by window of length win_length_secs and then padded with zeros to match n_fft_secs.",
+            )
+
+        self.parser.add_argument(
+            "--hop_length_secs", 
+            type = float,
+            default = feature_extractor_default_settings['hop_length_secs'],
+            help = "Hop length (in seconds).",
+            )
+
+        self.parser.add_argument(
+            "--pre_emph_coef", 
+            type = float,
+            default = feature_extractor_default_settings['pre_emph_coef'],
+            help = "Pre-emphasis coefficient.",
+            )
+
+        self.parser.add_argument(
+            "--n_mels", 
+            type = int,
+            default = feature_extractor_default_settings['n_mels'],
+            help = "Number of Mel bands to generate.",
+            )
+
+        self.parser.add_argument(
+            "--verbose", 
+            action = "store_true",
+            help = "Increase output verbosity.",
+            )
+
+
     def main(self):
 
-        self.extract_features()
-        
+        self.add_parser_args()
+        self.arguments = self.parser.parse_args()
+
 
 if __name__=="__main__":
 
-    instance = FeatureExtractor()
-    instance.main()
+    args_parser = ArgsParser()
+    args_parser.main()
+    parameters = args_parser.arguments
+
+    feature_extractor = FeatureExtractor(parameters)
+    feature_extractor.main()
     
---
+++ scripts/feature_extractor/settings.py
@@ -0,0 +1,10 @@
+feature_extractor_default_settings = {
+    'audio_paths_file_dir' : 'scripts/feature_extractor/feature_extractor_paths.lst',
+    'sampling_rate' : 16000,
+    'n_fft_secs': 0.023,
+    'window' : 'hamming',
+    'win_length_secs' : 0.023,
+    'hop_length_secs' : 0.010,
+    'pre_emph_coef' : 0.97,
+    'n_mels' : 80,
+}
