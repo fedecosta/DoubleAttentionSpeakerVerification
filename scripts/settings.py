4de950dba9f147237455a55c9016a9b1c97ad852
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2022-08-25 13:05:23 +0200

    settings with hardcoded variables

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,4 +1,10 @@
-feature_extractor_default_settings = {
+PATHS_GENERATOR_DEFAULT_SETTINGS = {
+    'dump_file_name' : 'feature_extractor_paths.lst',
+    'dump_data_dir' : 'scripts/feature_extractor/',
+    'valid_audio_formats': ['wav', 'm4a'],
+}
+
+FEATURE_EXTRACTOR_DEFAULT_SETTINGS = {
     'audio_paths_file_dir' : 'scripts/feature_extractor/feature_extractor_paths.lst',
     'sampling_rate' : 16000,
     'n_fft_secs': 0.023,
@@ -9,8 +15,12 @@ feature_extractor_default_settings = {
     'n_mels' : 80,
 }
 
-paths_generator_default_settings = {
-    'dump_file_name' : 'feature_extractor_paths.lst',
-    'dump_data_dir' : 'scripts/feature_extractor/',
-    'valid_audio_formats': ['wav', 'm4a'],
+LABELS_GENERATOR_DEFAULT_SETTINGS = {
+    'train_labels_dump_file_name' : 'scripts/labels/train/train_labels.ndx',
+    'valid_impostors_labels_dump_file_name' : 'scripts/labels/valid/valid_impostors_labels.ndx',
+    'valid_clients_labels_dump_file_name' : 'scripts/labels/valid/valid_clients_labels.ndx',
+    'train_speakers_pctg': 0.7,
+    'random_split' : True,
+    'clients_lines_max' : None,
+    'impostors_lines_max' : None,
 }
