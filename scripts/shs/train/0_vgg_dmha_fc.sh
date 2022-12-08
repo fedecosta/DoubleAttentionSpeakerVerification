5a65289ed4f8851ce193df42ef4c7a8e75460a3d
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2022-12-08 20:20:52 +0100

    added shs

---
+++ scripts/shs/train/0_vgg_dmha_fc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --output=logs/sbatch_output/slurm-%j.out
+#SBATCH -p veu             # Partition to submit to
+#SBATCH -c1
+#SBATCH --mem=64G      # Max CPU Memory
+#SBATCH --gres=gpu:4
+#SBATCH --job-name=train_vgg_dmha_fc
+python scripts/train.py \
+	--train_labels_path './labels/train/voxceleb_2/train_labels.ndx' \
+	--train_data_dir './datasets/voxceleb_2/dev/22_12_05_21_59_21_f0yycx91_azure-pine-7/' \
+	--valid_clients_path './labels/valid/voxceleb_2/clients.ndx' \
+	--valid_impostors_path './labels/valid/voxceleb_2/impostors.ndx' \
+	--valid_data_dir './datasets/voxceleb_2/dev/22_12_05_21_59_21_f0yycx91_azure-pine-7/' \
+	--model_output_folder './models/' \
+	--max_epochs 100 \
+	--batch_size 64 \
+	--eval_and_save_best_model_every 8000 \
+	--print_training_info_every 100 \
+	--early_stopping 35 \
+	--update_optimizer_every 0 \
+	--normalization 'cmn' \
+	--model_name_prefix 'vgg_dmha_fc' \
+	--embedding_size 400 \
+	--front_end 'VGGNL' \
+	--pooling_method 'DoubleMHA' \
+	--pooling_heads_number 32 \
+	--pooling_mask_prob 0.3 \
+	> logs/console_output/train/0_vgg_dmha_fc.log 2>&1
