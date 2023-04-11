3b44d37e0df50330a74348171456d51120b56baf
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2023-04-11 23:23:39 +0200

    shs experiments

--- scripts/shs/train/4_0_vgg_tmh_ap_fc.sh
+++ scripts/shs/train/4_0_vgg_tmh_ap_fc.sh
@@ -2,16 +2,16 @@
 #SBATCH -o logs/sbatch/outputs/slurm-%j.out
 #SBATCH -e logs/sbatch/errors/slurm-%j.err
 #SBATCH -p veu             # Partition to submit to
-#SBATCH -c1
+#SBATCH -c4
 #SBATCH --mem=64G      # Max CPU Memory
 #SBATCH --gres=gpu:4
 #SBATCH --job-name=train_vgg_tmh_ap_fc
 python scripts/train.py \
 	--train_labels_path './labels/train/voxceleb_2/22_12_26_00_36_23_39bjucvr_gallant-dragon-44/train_labels.ndx' \
-	--train_data_dir './datasets/voxceleb_2/dev/22_12_05_21_59_21_f0yycx91_azure-pine-7/' \
+	--train_data_dir '/home/usuaris/veussd/federico.costa/datasets/voxceleb_2/dev/22_12_05_21_59_21_f0yycx91_azure-pine-7/' \
 	--valid_clients_path './labels/valid/voxceleb_2/22_12_26_12_41_44_473354ug_dandy-darkness-45/clients.ndx' \
 	--valid_impostors_path './labels/valid/voxceleb_2/22_12_26_12_43_22_3jijl4ik_celestial-flower-46/impostors.ndx' \
-	--valid_data_dir './datasets/voxceleb_2/dev/22_12_05_21_59_21_f0yycx91_azure-pine-7/' \
+	--valid_data_dir '/home/usuaris/veussd/federico.costa/datasets/voxceleb_2/dev/22_12_05_21_59_21_f0yycx91_azure-pine-7/' \
 	--model_output_folder './models/' \
 	--max_epochs 200 \
 	--batch_size 128 \
